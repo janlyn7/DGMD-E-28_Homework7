1ab0cdeba23589b9d158adac8a139df556c26bc3
Janine <jlyn@cfa.harvard.edu>
2023-04-28 08:37:58 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {BrowserRouter as Router} from 'react-router-dom';
+import {GuessApp} from './app_guess.js';
+
+function App() {
+
+  return (
+    < Router >
+        < GuessApp/>
+    < /Router >
+  );
+}
+
+export default App;
